f98b6c32fb42db4a72a17a9dd50b9d56f959d3fe
paul degoes <pauldegoes@pauls-MacBook-Pro.local>
2017-09-21 15:46:23 -0600

    Move Regexp logic into its own testable space, and add tests for a handful of known use cases

---
+++ test/models/regular_expressions_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+
+class RegularExpressionsTest < ActiveSupport::TestCase
+  test "UPDATE_KARMA_BY_NAME matches a ++ with an @" do
+
+    assert_not_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("@alan ++")
+  end
+
+  test "UPDATE_KARMA_BY_NAME matches an ++ without an @" do
+
+    assert_not_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("alan ++")
+  end
+
+  test "UPDATE_KARMA_BY_NAME matches a -- with an @" do
+
+    assert_not_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("@alan --")
+  end
+
+  test "UPDATE_KARMA_BY_NAME matches an -- without an @" do
+
+    assert_not_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("alan --")
+  end
+
+  test "UPDATE_KARMA_BY_NAME does not match unless the string starts with something like a username" do
+    assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("Blah blah blah --")
+    assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("Blah blah blah ++")
+  end
+
+
+  test "UPDATE_KARMA_BY_NAME does not match when the string doesn't end with the ++ or -- operator" do
+    assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop -- ")
+    assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop ++ ")
+    assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop +++ ")
+    assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop --- ")
+  end
+
+end
