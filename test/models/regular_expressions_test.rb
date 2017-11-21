b677352c3595352519121f46b2ba1ed2f899364f
Paul De Goes <pauldegoes@hotmail.com>
2017-11-21 08:40:41 -0700

    Hook up new regx (#8)
    * Use new RegX for bot update by name
    
    * remove log from revision control

--- test/models/regular_expressions_test.rb
+++ test/models/regular_expressions_test.rb
@@ -1,6 +1,5 @@
 require 'test_helper'
 
-
 class RegularExpressionsTest < ActiveSupport::TestCase
   test "UPDATE_KARMA_BY_NAME matches a ++ with an @" do
 
@@ -27,12 +26,10 @@ class RegularExpressionsTest < ActiveSupport::TestCase
     assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("Blah blah blah ++")
   end
 
-
   test "UPDATE_KARMA_BY_NAME does not match when the string doesn't end with the ++ or -- operator" do
     assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop -- ")
     assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop ++ ")
     assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop +++ ")
     assert_nil RegularExpressions::UPDATE_KARMA_BY_NAME.match("stop --- ")
   end
-
 end
